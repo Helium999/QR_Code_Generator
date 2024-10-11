8fc11011b99b54b3cb451911da9b80f3c49137b8
Helium999 <adityanegi363@gmail.com>
2024-10-11 23:09:32 +0530

    Add small feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import qrcode as qr
 from PIL import Image
 from time import sleep
+from sys import exit
 
 #Designing the QR Code
 qr_code = qr.QRCode(
@@ -48,23 +49,40 @@ while True:
 img = qr_code.make_image(fill_color=qr_colour, back_color=qr_bg_colour)
 
 #Taking the input for file name of the image
-while True:
-    name = input("Enter the name of the QR code to save the image: ").strip()
+i = 1
+while i == 1:
+    name = input("\nEnter the name of the QR code to save the image: ").strip()
     if name.endswith(".png") or name.endswith(".jpg") or name.endswith(".jpeg") or name.endswith(".tiff"):
+        if not name.endswith(".png"):
+            print("\nOnly .png format supported")
         reverse_name = name[::-1]
-        reverse_name = reverse_name.replace("gnp.", "")
-        reverse_name = reverse_name.replace("gpj.", "")
-        reverse_name = reverse_name.replace("gepj.", "")
-        reverse_name = reverse_name.replace("ffit.", "")
+        reverse_name = reverse_name.replace("gnp.", "", 1)
+        reverse_name = reverse_name.replace("gpj.", "", 1)
+        reverse_name = reverse_name.replace("gepj.", "", 1)
+        reverse_name = reverse_name.replace("ffit.", "", 1)
         name = reverse_name[::-1]
 
-    confirm_save = input(f"Are you sure you want to save your QR code image as {name}.png (y/n): ")
-    if confirm_save == "y" or confirm_save == "yes":
-        break
-    elif confirm_save == "n" or confirm_save == "no":
-        print("The image was not saved by the previously entered name. Enter a new name:")
-    else:
-        print("Invalid input! Enter \"y\" to confirm or \"n\" to cancel.")
+#Confirmation for saving the file with the input name
+    j = 1
+    while j == 1:
+        confirm_save = input(f"Are you sure you want to save your QR code image as {name}.png (y/n): ")
+        if confirm_save == "y" or confirm_save == "yes":
+            i = 0
+            j = 0
+        elif confirm_save == "n" or confirm_save == "no":
+            print("\nThe image was not saved by the previously entered name.")
+            k = 1
+            while k == 1:
+                choice = input("To provide a new name, enter \"n\".\nTo exit the program completely, enter \"e\": ")
+                if choice == "n" or choice == "new":
+                    j = 0
+                    k = 0
+                elif choice == "e" or choice == "exit":
+                    exit()
+                else:
+                    print("\nInvalid input!")
+        else:
+            print("\nInvalid input! Enter \"y\" to confirm or \"n\" to cancel.")
 
 #Saving the image
 img.save(f"{name}.png")
@@ -80,4 +98,3 @@ print("1...")
 
 open_img = Image.open(f"{name}.png")
 open_img.show()
-
